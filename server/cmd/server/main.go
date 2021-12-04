984c24f673b8b3621727084e350984485682b2a0
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-04 21:36:21 +0300

    seerver/server.go: Delete unused prints

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -16,42 +16,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// fmt.Println("----CONFIGS")
-	// fmt.Printf("%+v\n", config)
-
 	mongo, err := mongo.Connect(config.DB)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// fmt.Println("----MONGO CONNECT")
-	// fmt.Printf("%+v\n", mongo)
 
 	repo, err := repo.New(mongo)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// fmt.Println("----REPO")
-	// fmt.Printf("%+v\n", repo)
-
 	services := services.New(repo)
-
-	// fmt.Println("----SERVICES")
-	// fmt.Printf("%+v\n", services)
 
 	srv, err := server.New(config.Server, services)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// fmt.Println("----SERVER")
-	// fmt.Printf("%+v\n", srv)
-
 	err = srv.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("SERVER STARTED")
 
 }
