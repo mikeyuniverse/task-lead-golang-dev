34d1d85cb9b79cca2986f98c1302707187e78361
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-04 21:40:13 +0300

    server/services.go: Create Repository interface. Delete useless error handlers.

--- server/internal/server/services/services.go
+++ server/internal/server/services/services.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"grpc-practice/internal/server/models"
-	"grpc-practice/internal/server/repo"
 	"grpc-practice/pkg/proto/transport"
 	"io"
 	"net/http"
@@ -14,27 +13,27 @@ import (
 	"strings"
 )
 
-type Services struct {
-	repo *repo.Repo
+type Repository interface {
+	GetItemsWithSort(int32, int32, string, string) ([]models.Item, error)
+	UpdateItems([]models.Item) error
 }
 
-func New(repo *repo.Repo) *Services {
+type Services struct {
+	repo Repository
+}
+
+func New(repo Repository) *Services {
 	return &Services{
 		repo: repo,
 	}
 }
 
 func (s *Services) Fetch(req *transport.FetchRequest) error {
-	items, err := s.GetDataByURL(req.Url)
+	items, err := s.GetItemsByURL(req.Url)
 	if err != nil {
 		return err
 	}
-	fmt.Printf("ITEMS ACCEPTED - %d\n", len(items))
-	err = s.UpdateDB(items)
-	if err != nil {
-		return err
-	}
-	return nil
+	return s.updateAllItems(items)
 }
 
 func (s *Services) List(context context.Context, start int32, limit int32, sortType string, orderType string) ([]*transport.Item, error) {
@@ -65,7 +64,7 @@ func (s *Services) List(context context.Context, start int32, limit int32, sortT
 	return result, nil
 }
 
-func (s *Services) GetDataByURL(url string) ([]models.Item, error) {
+func (s *Services) GetItemsByURL(url string) ([]models.Item, error) {
 	resp, err := http.Get(url)
 	if err != nil {
 		return []models.Item{}, err
@@ -82,17 +81,19 @@ func (s *Services) GetDataByURL(url string) ([]models.Item, error) {
 
 	for {
 		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
+
 		if err != nil {
+			if err == io.EOF {
+				break
+			}
 			return []models.Item{}, err
 		}
+
 		record = strings.Split(record[0], ";")
 		if len(record) != 2 {
-			fmt.Println("LEN RECORD NOT 2 - ", record)
 			continue
 		}
+
 		name := record[0]
 		price, err := strconv.Atoi(record[1])
 		if err != nil {
@@ -108,6 +109,6 @@ func (s *Services) GetDataByURL(url string) ([]models.Item, error) {
 	return data, nil
 }
 
-func (s *Services) UpdateDB(items []models.Item) error {
+func (s *Services) updateAllItems(items []models.Item) error {
 	return s.repo.UpdateItems(items)
 }
