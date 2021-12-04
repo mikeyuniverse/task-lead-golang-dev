8f4b2a700b9068020328ffc5b6ca01c66b976587
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-04 21:37:37 +0300

    server/repo.go: Delete unused prints

--- server/internal/server/repo/repo.go
+++ server/internal/server/repo/repo.go
@@ -3,7 +3,6 @@ package repo
 import (
 	"grpc-practice/internal/server/models"
 	"grpc-practice/internal/server/repo/mongo"
-	"log"
 )
 
 type Repo struct {
@@ -16,32 +15,26 @@ func New(db *mongo.MongoDB) (*Repo, error) {
 
 func (r *Repo) UpdateItems(items []models.Item) error {
 	for _, item := range items {
+
 		if item.Name == "" && item.Price == 0 {
 			continue
 		}
+
 		name := item.Name
 		price := item.Price
 
 		itemDB, err := r.db.GetItemByName(name)
 		if err != nil {
-			log.Println(err)
 			continue
 		}
 
 		if itemDB == nil {
-			err = r.db.CreateItem(item)
-			if err != nil {
-				log.Println(err)
-			}
+			r.db.CreateItem(item)
 			continue
 		}
 
 		if item.Price != price {
-			log.Printf("CHANGE PRICE %d --> %d", itemDB.Price, item.Price)
-			err = r.db.UpdatePriceByName(item)
-			if err != nil {
-				log.Println(err)
-			}
+			r.db.UpdatePriceByName(item)
 			continue
 		}
 	}
@@ -49,6 +42,5 @@ func (r *Repo) UpdateItems(items []models.Item) error {
 }
 
 func (r *Repo) GetItemsWithSort(start int32, limit int32, sortType string, orderType string) ([]models.Item, error) {
-
 	return r.db.GetItemsWithSort(start, limit, sortType, orderType)
 }
