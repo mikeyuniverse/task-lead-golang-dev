39b0c5ab3d9962bb5ba54690cfbde5fe20d17329
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-04 21:05:26 +0300

    Write client side main.go

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -1,38 +1,48 @@
 package main
 
 import (
-	"grpc-client/internal/models"
+	"fmt"
 	"grpc-client/internal/server"
 	"log"
+	"time"
 )
 
 const url = "http://164.92.251.245:8080/api/v1/products"
-const limit = 5     // List method request count
-const itemLimit = 4 // Products count in one request
+
+// const limit = 5     // List method request count
+// const itemLimit = 4 // Products count in one request
 
 func main() {
 
-	server := server.New()
-
-	pagging := models.Pagging{
-		Limit: limit,
-	}
-
-	sorting := models.Sorting{
-		SortingType: models.SortingType{Alphabet: true},
-		OrderType:   models.OrderType{Ascending: true},
+	server, err := server.New()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// Download and update data in MongoDB
-	server.Fetch(url)
+	for {
+		time.Sleep(time.Second * 3)
+		fmt.Println(server.Fetch(url))
+	}
+
+	// pagging := models.Pagging{
+	// 	Limit: limit,
+	// }
+
+	// sorting := models.Sorting{
+	// 	SortingType: models.SortingType{Alphabet: true},
+	// 	OrderType:   models.OrderType{Ascending: true},
+	// }
 
 	// Get pagging data
-	for i := 0; i < limit; i++ {
-		list, err := server.List(&pagging, &sorting)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		log.Println(list)
-	}
+	// for i := 0; i < limit; i++ {
+	// for {
+	// 	time.Sleep(time.Second * 5)
+	// 	list, err := server.List(&pagging, &sorting)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 		continue
+	// 	}
+	// 	log.Println(list)
+	// }
 }
