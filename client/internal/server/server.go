751b2e1b6dea90a438537d5fb4512d6f4203f6e3
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-04 21:05:42 +0300

    Implemented client side server.go

--- client/internal/server/server.go
+++ client/internal/server/server.go
@@ -1,14 +1,34 @@
 package server
 
-import "grpc-client/internal/models"
+import (
+	"context"
+	"grpc-client/internal/models"
+	"grpc-client/pkg/proto/transport"
 
-type Server struct{}
+	"google.golang.org/grpc"
+)
 
-func New() *Server {
-	return &Server{}
+type Server struct {
+	grpcClient transport.FetchServiceClient
 }
 
-func (s *Server) Fetch(url string) {}
+func New() (*Server, error) {
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	client := transport.NewFetchServiceClient(conn)
+
+	return &Server{
+		grpcClient: client,
+	}, nil
+}
+
+func (s *Server) Fetch(url string) error {
+	_, err := s.grpcClient.Fetch(context.Background(), &transport.FetchRequest{Url: url})
+	return err
+}
 
 func (s *Server) List(pagg *models.Pagging, sort *models.Sorting) ([]string, error) {
 	return []string{}, nil
