baa11e0bb0911cae47edd4811096f313f489b290
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-10 13:01:07 +0300

    server/repo.go: Del implementation db. Add constructor products entity repository.

--- internal/server/repo/repo.go
+++ internal/server/repo/repo.go
@@ -2,51 +2,26 @@ package repo
 
 import (
 	"grpc-practice/internal/server/models"
+
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-type Database interface {
+var collectionProducts = "products"
+
+type Products interface {
+	UpdateItems(items []models.Item) error
 	GetItemByName(name string) (*models.Item, error)
 	CreateItem(item models.Item) error
 	UpdatePriceByName(item models.Item) error
-	GetItemsWithSort(start int32, limit int32, sortType string, orderType string) ([]models.Item, error)
+	GetItemsWithSort(params ListParams) ([]models.Item, error)
 }
 
 type Repo struct {
-	db Database
+	Products Products
 }
 
-func New(db Database) (*Repo, error) {
-	return &Repo{db: db}, nil
-}
-
-func (r *Repo) UpdateItems(items []models.Item) error {
-	for _, item := range items {
-
-		if item.Name == "" && item.Price == 0 {
-			continue
-		}
-
-		name := item.Name
-		price := item.Price
-
-		itemDB, err := r.db.GetItemByName(name)
-		if err != nil {
-			continue
-		}
-
-		if itemDB == nil {
-			r.db.CreateItem(item)
-			continue
-		}
-
-		if item.Price != price {
-			r.db.UpdatePriceByName(item)
-			continue
-		}
-	}
-	return nil
-}
-
-func (r *Repo) GetItemsWithSort(start int32, limit int32, sortType string, orderType string) ([]models.Item, error) {
-	return r.db.GetItemsWithSort(start, limit, sortType, orderType)
+func New(db *mongo.Database) (*Repo, error) {
+	return &Repo{
+		Products: newProductsRepo(db),
+	}, nil
 }
