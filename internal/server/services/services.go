1af82867a5bf4986852fcbc90395eff1f86225eb
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-14 20:12:23 +0300

    services.go: Add getter pkg in Services struct. Create Getter interface. Delete getItemsByURL function.

--- internal/server/services/services.go
+++ internal/server/services/services.go
@@ -2,34 +2,35 @@ package services
 
 import (
 	"context"
-	"encoding/csv"
 	"errors"
-	"fmt"
 	"grpc-practice/internal/server/models"
 	"grpc-practice/internal/server/repo"
 	"grpc-practice/pkg/proto/transport"
-	"io"
-	"net/http"
-	"strconv"
-	"strings"
 )
 
-type Services struct {
-	repo *repo.Repo
+//go:generate mockgen -source=services.go -destination=mocks/mock.go
+
+type Getter interface {
+	GetItemsByURL(url string) ([]models.Item, error)
 }
 
-func New(repo *repo.Repo) *Services {
+type Services struct {
+	repo   *repo.Repo
+	getter Getter
+}
+
+func New(repo *repo.Repo, getter Getter) *Services {
 	return &Services{
 		repo: repo,
 	}
 }
 
 func (s *Services) Fetch(url string) error {
-	items, err := s.getItemsByURL(url)
+	items, err := s.getter.GetItemsByURL(url)
 	if err != nil {
 		return err
 	}
-	return s.updateAllItems(items)
+	return s.repo.Products.UpdateItems(items)
 }
 
 func (s *Services) List(context context.Context, params repo.ListParams) ([]*transport.Item, error) {
@@ -60,53 +61,4 @@ func (s *Services) List(context context.Context, params repo.ListParams) ([]*tra
 		result = append(result, item.ToPB())
 	}
 	return result, nil
-}
-
-func (s *Services) getItemsByURL(url string) ([]models.Item, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return []models.Item{}, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return []models.Item{}, fmt.Errorf("request error. code - %d", resp.StatusCode)
-	}
-
-	r := csv.NewReader(resp.Body)
-
-	data := make([]models.Item, 1)
-
-	for {
-		record, err := r.Read()
-
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return []models.Item{}, err
-		}
-
-		record = strings.Split(record[0], ";")
-		if len(record) != 2 {
-			continue
-		}
-
-		name := record[0]
-		price, err := strconv.Atoi(record[1])
-		if err != nil {
-			return []models.Item{}, err
-		}
-
-		data = append(data, models.Item{
-			Name:  name,
-			Price: price,
-		})
-	}
-
-	return data, nil
-}
-
-func (s *Services) updateAllItems(items []models.Item) error {
-	return s.repo.Products.UpdateItems(items)
 }
