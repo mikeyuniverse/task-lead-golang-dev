a5ee077955494992baa829aa986a0243179c33ba
Sergey Mikryukov <mikeyuniversezero@gmail.com>
2021-12-14 18:25:22 +0300

    server_test.go: Create test for List method of server

--- internal/server/server/server_test.go
+++ internal/server/server/server_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"grpc-practice/internal/server/config"
+	"grpc-practice/internal/server/repo"
 	mock_server "grpc-practice/internal/server/server/mocks"
 	"grpc-practice/pkg/proto/transport"
 	"testing"
@@ -61,4 +62,88 @@ func TestServer_Fetch(t *testing.T) {
 
 }
 
-func TestServer_List(t *testing.T) {}
+func TestServer_List(t *testing.T) {
+	testTable := []struct {
+		name    string
+		data_in struct {
+			sort    transport.SortType
+			pagging transport.Pagging
+		}
+		data_want struct {
+			itemCount int
+			wantErr   bool
+			items     transport.ListResponse
+			errorText error
+		}
+	}{
+		{
+			name: "OK",
+			data_in: struct {
+				sort    transport.SortType
+				pagging transport.Pagging
+			}{
+				sort: transport.SortType_NAME,
+				pagging: transport.Pagging{
+					Limit: 5,
+					Start: 0,
+				},
+			},
+			data_want: struct {
+				itemCount int
+				wantErr   bool
+				items     transport.ListResponse
+				errorText error
+			}{
+				itemCount: 5,
+				wantErr:   false,
+				items: transport.ListResponse{
+					Item: []*transport.Item{
+						{Name: "Item 1", Price: 100},
+						{Name: "Item 2", Price: 200},
+						{Name: "Item 3", Price: 300},
+						{Name: "Item 4", Price: 400},
+						{Name: "Item 5", Price: 500},
+					},
+				},
+				errorText: nil,
+			},
+		},
+	}
+
+	for _, test := range testTable {
+		// Initialize dependencies
+		ctx := context.Background()
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		services := mock_server.NewMockservicer(ctrl)
+		services.EXPECT().List(context.TODO(), repo.ListParams{
+			Start:     int64(test.data_in.pagging.Start),
+			Limit:     int64(test.data_in.pagging.Limit),
+			SortType:  test.data_in.sort.String(),
+			OrderType: transport.SortOrder_DESC.String(),
+		}).Return(test.data_want.items.Item, test.data_want.errorText)
+		server, err := New(&ctx, config.GRPC{Port: "9000"}, services)
+
+		if err != nil {
+			t.Error("error init server")
+		}
+
+		response, err := server.List(ctx, &transport.ListRequest{
+			Start:   test.data_in.pagging.Start,
+			Limit:   test.data_in.pagging.Limit,
+			Sort:    test.data_in.sort,
+			Pagging: transport.SortOrder_DESC,
+		})
+
+		if err != nil && !test.data_want.wantErr {
+			t.Fatalf("request error - %s\n", err)
+		}
+
+		if len(response.Item) != test.data_want.itemCount {
+			t.Fatalf("the expected quantity of goods does not match what was received\nWant items - %d\nGot - %d\n", test.data_in.pagging.Limit, len(response.Item))
+		}
+
+	}
+
+}
